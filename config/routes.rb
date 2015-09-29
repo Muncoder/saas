014e6ebd8db056dc3cb0dd6a57282a76270b8017
Munir <munirchittagong@yahoo.com>
2015-09-29 11:06:17 +0100

    Updated routes file for registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
   resources :plans
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :contacts
   get 'pages/home'
   get 'pages/about'
